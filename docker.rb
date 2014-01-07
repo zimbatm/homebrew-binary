9cbb003caab046c36aaa19c01a5357c296306198
Steeve Morin <steeve.morin@gmail.com>
2014-01-07 01:06:02 +0100

    docker 0.7.3 - any application as a container
    Docker is an open-source project to easily create lightweight, portable,
    self-sufficient containers from any application. The same container that a
    developer builds and tests on a laptop can run at scale, in production, on VMs,
    bare metal, OpenStack clusters, public clouds and more.
    
    Closes #48.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

---
+++ docker.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Docker < Formula
+  homepage 'http://www.docker.io/'
+  url 'https://get.docker.io/builds/Darwin/x86_64/docker-0.7.3.tgz'
+  sha1 '7c3f07213010c414fe60b88d58d89181b7a7eed1'
+
+  def install
+    bin.install 'local/bin/docker'
+  end
+
+  test do
+    system "#{bin}/docker"
+  end
+end
