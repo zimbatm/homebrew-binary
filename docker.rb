8d29f40d369a88a3d58b8847f8976e6db0f1d6f6
Misty De Meo <mistydemeo@gmail.com>
2014-01-07 22:45:37 -0800

    docker: add 32-bit binary

--- docker.rb
+++ docker.rb
@@ -2,10 +2,16 @@ require 'formula'
 
 class Docker < Formula
   homepage 'http://www.docker.io/'
-  url 'https://get.docker.io/builds/Darwin/x86_64/docker-0.7.3.tgz'
-  sha1 '7c3f07213010c414fe60b88d58d89181b7a7eed1'
 
-  depends_on :arch => :x86_64
+  if MacOS.prefer_64_bit?
+    url 'https://get.docker.io/builds/Darwin/x86_64/docker-0.7.3.tgz'
+    sha1 '7c3f07213010c414fe60b88d58d89181b7a7eed1'
+  else
+    url 'https://get.docker.io/builds/Darwin/i386/docker-0.7.3.tgz'
+    sha1 '1f0449f70c3d11f2cd326ca380af31e35928dca2'
+  end
+
+  depends_on :arch => :intel
 
   def install
     bin.install 'local/bin/docker'
