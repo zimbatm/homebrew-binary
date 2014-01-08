8c6c4f023e2a33fade8baa1798a0ae8bfd8c2851
Misty De Meo <mistydemeo@gmail.com>
2014-01-07 22:40:04 -0800

    docker: x86_64 only

--- docker.rb
+++ docker.rb
@@ -5,6 +5,8 @@ class Docker < Formula
   url 'https://get.docker.io/builds/Darwin/x86_64/docker-0.7.3.tgz'
   sha1 '7c3f07213010c414fe60b88d58d89181b7a7eed1'
 
+  depends_on :arch => :x86_64
+
   def install
     bin.install 'local/bin/docker'
   end
