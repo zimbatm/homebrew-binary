88e19bac2f99c2d56e22149ffec5e1157dfdc748
Simon Pantzare <simon@pewpewlabs.com>
2014-02-05 21:19:34 +0100

    docker 0.8.0
    Closes #67.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- docker.rb
+++ docker.rb
@@ -4,11 +4,11 @@ class Docker < Formula
   homepage 'https://www.docker.io/'
 
   if MacOS.prefer_64_bit?
-    url 'https://get.docker.io/builds/Darwin/x86_64/docker-0.7.4.tgz'
-    sha1 '4503c6f762cdc8310e3bb43babbb7f90b45697f7'
+    url 'https://get.docker.io/builds/Darwin/x86_64/docker-0.8.0.tgz'
+    sha1 'ab78fe14a005d4168a6c89f708a1bcd24a2707d6'
   else
-    url 'https://get.docker.io/builds/Darwin/i386/docker-0.7.4.tgz'
-    sha1 'b21bfc0eafddcadd40218c138ec4a4272adc7e93'
+    url 'https://get.docker.io/builds/Darwin/i386/docker-0.8.0.tgz'
+    sha1 '15fa9744ac56243367c162353d7f109fdfe54fec'
   end
 
   depends_on :arch => :intel
