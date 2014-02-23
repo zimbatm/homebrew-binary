636cca5f667c7ce2c8a1616ed7b74af279704a4f
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2014-02-24 01:44:58 +0200

    Grenchman 0.2 (new formula)
    Closes #75.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ grenchman.rb
@@ -0,0 +1,14 @@
+require "formula"
+
+class Grenchman < Formula
+  homepage "http://leiningen.org/grench.html"
+  url "https://grenchman.s3.amazonaws.com/downloads/grench-0.2.0-mac"
+  sha1 "a1468c2e31deeeaf9925c6a3d1ebf9b3aac96521"
+
+  depends_on :arch => :x86_64
+  depends_on "libffi"
+
+  def install
+    bin.install "grench-0.2.0-mac" => "grench"
+  end
+end
