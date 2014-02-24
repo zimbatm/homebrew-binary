e73fdc12637c94aba06aba22d0de01f5ec5f55b6
scottsuch <sgorsuch@gmail.com>
2014-02-24 09:39:32 -0500

    Packer 0.5.2
    Closes #76.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- packer.rb
+++ packer.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Packer < Formula
   homepage 'http://www.packer.io'
-  version '0.5.1'
+  version '0.5.2'
 
   if Hardware.is_64_bit?
-    url 'https://dl.bintray.com/mitchellh/packer/0.5.1_darwin_amd64.zip'
-    sha256 '56bec31f0d3540d566ef86979b25367660d7e72c010c9d87ef91c5c2138e9eae'
+    url 'https://dl.bintray.com/mitchellh/packer/0.5.2_darwin_amd64.zip'
+    sha256 '1c3c881c4a29cc71617a87503557df684977d86b966f5ed0273298e2f6221594'
   else
-    url 'https://dl.bintray.com/mitchellh/packer/0.5.1_darwin_386.zip'
-    sha256 'a0d8db4944d0024af05e256357cad014662eddefef67b1b2fe8a5060659a5be2'
+    url 'https://dl.bintray.com/mitchellh/packer/0.5.2_darwin_386.zip'
+    sha256 '32849dcbbb83b86d0d3f621705224ee580d3215f4060913f0bcbc974035ffd00'
   end
 
   depends_on :arch => :intel
