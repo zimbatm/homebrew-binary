3573689e5640de70769d627db636dca006727db2
Sean Lydon <lydon.sean@gmail.com>
2014-04-10 15:04:22 -0700

    Update p4 checksums
    Fixes #89.
    Closes #90.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- perforce.rb
+++ perforce.rb
@@ -6,11 +6,11 @@ class Perforce < Formula
   if MacOS.prefer_64_bit?
     url 'http://filehost.perforce.com/perforce/r14.1/bin.macosx105x86_64/p4'
     version '2014.1.815009'
-    sha1 '92f0a9a7d628d8c729b7096f2b042a3b857e1f8e'
+    sha1 '9a7e43b887066a087da71c238bcd78a00b065e6a'
   else
     url 'http://filehost.perforce.com/perforce/r14.1/bin.macosx105x86/p4'
     version '2014.1.815009'
-    sha1 '5fbd350b9950a7f468c40139c6a323a775f67e35'
+    sha1 '990d7ced3ca2b16cb6aeeb4d6083f11c260857af'
   end
 
   def install
