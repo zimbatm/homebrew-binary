d3f5e6646444aacb44cdf15cba39678ca129b14b
Jack Nagel <jacknagel@gmail.com>
2014-04-16 16:35:01 -0500

    exercism: drop VERSION constant
    We typically avoid this in order to keep URLs copy/paste-able. The
    "version" method can be used in instance methods.

--- exercism.rb
+++ exercism.rb
@@ -1,16 +1,14 @@
 require 'formula'
 
 class Exercism < Formula
-  VERSION = '1.5.1'.freeze
-
   homepage 'http://cli.exercism.io'
-  version VERSION
+  version '1.5.1'
 
   if Hardware.is_64_bit?
-    url "https://github.com/exercism/cli/releases/download/v#{VERSION}/exercism-darwin-amd64.tgz"
+    url "https://github.com/exercism/cli/releases/download/v1.5.1/exercism-darwin-amd64.tgz"
     sha1 '4d19db279ae6752b6032b2a9e96cfc53981eff8f'
   else
-    url "https://github.com/exercism/cli/releases/download/v#{VERSION}/exercism-darwin-386.tgz"
+    url "https://github.com/exercism/cli/releases/download/v1.5.1/exercism-darwin-386.tgz"
     sha1 'cbe9697c1133e854832d620d52970fb3d198ba4f'
   end
 
@@ -19,6 +17,6 @@ class Exercism < Formula
   end
 
   test do
-    assert_equal "exercism version #{VERSION}", `#{bin}/exercism -v`.strip
+    assert_equal "exercism version #{version}", `#{bin}/exercism -v`.strip
   end
 end
