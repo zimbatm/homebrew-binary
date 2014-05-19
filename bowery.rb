dd527dab843f1c9eefb15879a0c928fbe821e9ff
Vincent Klaiber <vincentklaiber@users.noreply.github.com>
2014-05-19 08:13:24 +0200

    bowery 2.2.2
    Closes #105.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- bowery.rb
+++ bowery.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Bowery < Formula
   homepage 'http://bowery.io'
-  url 'http://download.bowery.io/downloads/bowery_2.2.1_darwin_amd64.zip'
-  version '2.2.1'
-  sha1 'ecb1947c7507c6199d0de2316eca019f02db4e37'
+  url 'http://download.bowery.io/downloads/bowery_2.2.2_darwin_amd64.zip'
+  version '2.2.2'
+  sha1 '2edf51770a988174385da031d924c1741f3299d3'
 
   depends_on :arch => :x86_64
 
