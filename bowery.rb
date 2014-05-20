f4c3e462b61cf265d66b3c1c9bde2e06bba407a9
Vincent Klaiber <vincent@schimpanz.com>
2014-05-20 20:17:06 +0200

    bowery 2.2.3
    Includes 32bit support.
    
    Closes #107.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- bowery.rb
+++ bowery.rb
@@ -2,11 +2,15 @@ require 'formula'
 
 class Bowery < Formula
   homepage 'http://bowery.io'
-  url 'http://download.bowery.io/downloads/bowery_2.2.2_darwin_amd64.zip'
-  version '2.2.2'
-  sha1 '2edf51770a988174385da031d924c1741f3299d3'
+  version '2.2.3'
 
-  depends_on :arch => :x86_64
+  if Hardware.is_64_bit?
+    url 'http://download.bowery.io/downloads/bowery_2.2.3_darwin_amd64.zip'
+    sha1 '9e15ee579b1ad78615c2e103fb2efc2e32dd5a2a'
+  else
+    url 'http://download.bowery.io/downloads/bowery_2.2.3_darwin_386.zip'
+    sha1 '948b39c8076c6c978d04e876f1ffcc00d19e584c'
+  end
 
   def install
     bin.install 'bowery'
