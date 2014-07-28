1a82ee8991658e21e56e7709dbed268110adb808
cromulus <bill.cromie@gmail.com>
2014-07-28 16:21:54 -0400

    consul 0.3.1
    Closes #123.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- consul.rb
+++ consul.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Consul < Formula
   homepage "http://www.consul.io"
-  url "https://dl.bintray.com/mitchellh/consul/0.2.0_darwin_amd64.zip"
-  sha1 "fc39e649d246ea5e3c39269301fdf72ed1d2fd6b"
-  version "0.2.0"
+  url "https://dl.bintray.com/mitchellh/consul/0.3.1_darwin_amd64.zip"
+  sha1 "8f28c69bd15a99c2105cf2f5f35a564643054296"
+  version "0.3.1"
 
   depends_on :arch => :x86_64
 
