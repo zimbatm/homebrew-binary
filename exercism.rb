fb7b304e87e0caa17c4986380b464d7c48913104
Katrina Owen <katrina.owen@gmail.com>
2014-08-28 08:12:53 -0700

    exercism 1.7.0
    Closes #143.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- exercism.rb
+++ exercism.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Exercism < Formula
   homepage 'http://cli.exercism.io'
-  version '1.6.2'
+  version '1.7.0'
 
   if Hardware.is_64_bit?
-    url "https://github.com/exercism/cli/releases/download/v1.6.2/exercism-darwin-amd64.tgz"
-    sha1 '6934fc9fbda27f69cb80b4e5999f7f2c83220e11'
+    url "https://github.com/exercism/cli/releases/download/v1.7.0/exercism-darwin-amd64.tgz"
+    sha1 '60fdf5a6a83074e59935561fdf23cbf1adad68ef'
   else
-    url "https://github.com/exercism/cli/releases/download/v1.6.2/exercism-darwin-386.tgz"
-    sha1 'e32ce61bcd5aae81164a800f2aa75b8ef62b5ab3'
+    url "https://github.com/exercism/cli/releases/download/v1.7.0/exercism-darwin-386.tgz"
+    sha1 '11d310b9acd0aa85151d4ec2badf26a9128dcf18'
   end
 
   def install
