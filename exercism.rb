739dce8a820fdae8cdb5535c03a6325a5b22639b
Katrina Owen <katrina.owen@gmail.com>
2014-09-26 07:55:29 -0700

    exercism 1.7.3
    Handle exercism home directories that are symlinks
    
    Closes #152.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- exercism.rb
+++ exercism.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Exercism < Formula
   homepage 'http://cli.exercism.io'
-  version '1.7.2'
+  version '1.7.3'
 
   if Hardware.is_64_bit?
-    url "https://github.com/exercism/cli/releases/download/v1.7.2/exercism-mac-64bit.tgz"
-    sha1 '5caa8f95fb50316df671c1c792cbcf7bc91297f2'
+    url "https://github.com/exercism/cli/releases/download/v1.7.3/exercism-mac-64bit.tgz"
+    sha1 'b297d77ff236bb211e147ab6d8891b71bb93f556'
   else
-    url "https://github.com/exercism/cli/releases/download/v1.7.2/exercism-mac-32bit.tgz"
-    sha1 'cbe9697c1133e854832d620d52970fb3d198ba4f'
+    url "https://github.com/exercism/cli/releases/download/v1.7.3/exercism-mac-32bit.tgz"
+    sha1 '9500fc5019002516814ee70da36f40741048154c'
   end
 
   def install
