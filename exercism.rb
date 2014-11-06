73f9e0233f0dbe3d3afb07d5faf1dbf3ee514261
Katrina Owen <katrina.owen@gmail.com>
2014-11-05 19:34:03 -0800

    exercism 1.8.2
    Closes #164.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- exercism.rb
+++ exercism.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Exercism < Formula
   homepage 'http://cli.exercism.io'
-  version '1.8.0'
+  version '1.8.2'
 
   if Hardware.is_64_bit?
-    url "https://github.com/exercism/cli/releases/download/v1.8.0/exercism-mac-64bit.tgz"
-    sha1 '233c2392289e1f9e202b907fa1b86328f5242cea'
+    url "https://github.com/exercism/cli/releases/download/v1.8.2/exercism-mac-64bit.tgz"
+    sha1 '2ef887286ab5b7be92cb754ae9adbd28c34e6335'
   else
-    url "https://github.com/exercism/cli/releases/download/v1.8.0/exercism-mac-32bit.tgz"
-    sha1 '01adcd7711949139e1932a0185d4759ad643416e'
+    url "https://github.com/exercism/cli/releases/download/v1.8.2/exercism-mac-32bit.tgz"
+    sha1 '8cd43e4838587969c688a9321acd97ed998bcd92'
   end
 
   def install
