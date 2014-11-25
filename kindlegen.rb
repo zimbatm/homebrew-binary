39dbb80922401d2b170f950809b4b03263ec91c8
Alex Burka <aburka@seas.upenn.edu>
2014-11-24 21:01:47 -0500

    kindlegen: fix sha1 again
    Closes #167.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- kindlegen.rb
+++ kindlegen.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Kindlegen < Formula
   homepage 'http://www.amazon.com/gp/feature.html?docId=1000765211'
   url 'http://kindlegen.s3.amazonaws.com/KindleGen_Mac_i386_v2_9.zip'
-  sha1 '94e959d10d51f4fe938b8ddab11bcc2c4dd26b39'
+  sha1 'efa5ad0b05650f7f71543535ea2b232bb5fec571'
 
   def install
     bin.install 'kindlegen'
