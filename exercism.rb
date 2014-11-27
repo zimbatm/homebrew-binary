575527d682dab5a3bb2460cd409c73327385456e
Katrina Owen <katrina.owen@gmail.com>
2014-11-27 15:27:52 -0800

    exercism 1.9.0
    This release adds a new command allowing users to easily get someone's
    solution to an exercism problem and run the tests or change the code.
    
    Closes #168.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- exercism.rb
+++ exercism.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Exercism < Formula
   homepage 'http://cli.exercism.io'
-  version '1.8.2'
+  version '1.9.0'
 
   if Hardware.is_64_bit?
-    url "https://github.com/exercism/cli/releases/download/v1.8.2/exercism-mac-64bit.tgz"
-    sha1 '2ef887286ab5b7be92cb754ae9adbd28c34e6335'
+    url "https://github.com/exercism/cli/releases/download/v1.9.0/exercism-mac-64bit.tgz"
+    sha1 '384d0f5776f1635e2c7eeff5477d9aa9db7ce11a'
   else
-    url "https://github.com/exercism/cli/releases/download/v1.8.2/exercism-mac-32bit.tgz"
-    sha1 '8cd43e4838587969c688a9321acd97ed998bcd92'
+    url "https://github.com/exercism/cli/releases/download/v1.9.0/exercism-mac-32bit.tgz"
+    sha1 '35ce83cdd7c532d2650abafe15f85312c738f50c'
   end
 
   def install
