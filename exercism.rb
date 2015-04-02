4e3814d21ea36543d9189955d384ce0df67a59b2
Katrina Owen <katrina.owen@gmail.com>
2015-04-01 20:17:51 -0700

    Update exercism CLI
    This fixes a panic that occurs under certain circumstances for a subset of users.
    For the affected users, this bug is a complete blocker to using the CLI.
    
    Closes #203.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- exercism.rb
+++ exercism.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Exercism < Formula
   homepage 'http://cli.exercism.io'
-  version '2.0.1'
+  version '2.0.2'
 
   if Hardware.is_64_bit?
-    url "https://github.com/exercism/cli/releases/download/v2.0.1/exercism-mac-64bit.tgz"
-    sha1 '0f4181178ffa74e2f12878cfc50b9d5a4032dfc0'
+    url "https://github.com/exercism/cli/releases/download/v2.0.2/exercism-mac-64bit.tgz"
+    sha1 '43d86a28cfd61fd78a3a6da4cec7f4554845854b'
   else
-    url "https://github.com/exercism/cli/releases/download/v2.0.1/exercism-mac-32bit.tgz"
-    sha1 '5f27e1d43de7df79480bd374e06028968db13a53'
+    url "https://github.com/exercism/cli/releases/download/v2.0.2/exercism-mac-32bit.tgz"
+    sha1 '875c252ce9fe4b807146d19af275b164284c7552'
   end
 
   def install
