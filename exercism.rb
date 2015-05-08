00009a719a0275c489a67c4bc4582300d227f9a6
Katrina Owen <katrina.owen@gmail.com>
2015-05-08 07:29:29 -0700

    Update exercism to newest minor version
    This release enhances some of the existing functionality,
    as well as fixing a couple of bugs.
    
    The CLI now:
    
    - respects '$XDG_CONFIG_HOME'
    - allows people to specify a target directory for the demo, instead of putting everything in ~/exercism/*
    - adds support for a '--test' flag to allow submitting a test file in the solution
    
    Bugfixes:
    
    - Handle different file encodings in submissions
    - Handle config as either directory or file
    - Fix nested path issue when downloading problems
    
    Closes #216.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- exercism.rb
+++ exercism.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Exercism < Formula
   homepage 'http://cli.exercism.io'
-  version '2.0.2'
+  version '2.1.0'
 
   if Hardware.is_64_bit?
-    url "https://github.com/exercism/cli/releases/download/v2.0.2/exercism-mac-64bit.tgz"
-    sha1 '43d86a28cfd61fd78a3a6da4cec7f4554845854b'
+    url "https://github.com/exercism/cli/releases/download/v2.1.0/exercism-mac-64bit.tgz"
+    sha1 'b5f883b7aed16441da6fd64af385c8162555fb9b'
   else
-    url "https://github.com/exercism/cli/releases/download/v2.0.2/exercism-mac-32bit.tgz"
-    sha1 '875c252ce9fe4b807146d19af275b164284c7552'
+    url "https://github.com/exercism/cli/releases/download/v2.1.0/exercism-mac-32bit.tgz"
+    sha1 'dfceb9dfe15df403b38b4c9af2c179f572a00913'
   end
 
   def install
