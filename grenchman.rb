d7004faf4ea76d017b37d8c2197e721cf18d922c
Larry Gilbert <larry@l2g.to>
2015-06-19 23:39:04 -0700

    grenchman: new url
    Closes #232.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- grenchman.rb
+++ grenchman.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class Grenchman < Formula
   homepage "http://leiningen.org/grench.html"
-  url "https://grenchman.s3.amazonaws.com/downloads/grench-0.2.0-mac"
-  sha1 "a1468c2e31deeeaf9925c6a3d1ebf9b3aac96521"
+  url "https://technomancy.github.io/grenchman/downloads/grench-0.2.0-mac"
+  sha256 "f70542dffc1f60e372b77582998570b02ff21c85f699fd1854c98e4359804bb6"
 
   depends_on :arch => :x86_64
   depends_on "libffi"
