d4d1015bcf68f7fd66ae216fd2f183610156387b
Sasha Gerrand <github-homebrew-binary@sgerrand.com>
2015-06-29 20:22:14 +0100

    Consul v0.5.2
    Upgrade to latest generally available release.
    
    Closes #235.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- consul.rb
+++ consul.rb
@@ -1,8 +1,8 @@
 class Consul < Formula
   homepage "http://www.consul.io"
-  url "https://dl.bintray.com/mitchellh/consul/0.5.0_darwin_amd64.zip"
-  sha256 "24d9758c873e9124e0ce266f118078f87ba8d8363ab16c2e59a3cd197b77e964"
-  version "0.5.0"
+  url "https://dl.bintray.com/mitchellh/consul/0.5.2_darwin_amd64.zip"
+  sha256 "87be515d7dbab760a61a359626a734f738d46ece367f68422b7dec9197d9eeea"
+  version "0.5.2"
 
   depends_on :arch => :x86_64
 
